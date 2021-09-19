eea391e5f30605cc103f13fed4b3af4c09d80c9b
Khondakar Readul Islam <khondakar22readul@gmail.com>
2021-09-19 21:17:08 +0200

    First React app Skeleton

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+
+
+function App() {
+  return (  
+    <div>
+    <h2>Let's get Started!</h2>
+    <ExpenseItem></ExpenseItem>
+    </div>
+   );
+}
+
+export default App;
